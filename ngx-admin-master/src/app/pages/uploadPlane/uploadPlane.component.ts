08cf9bab5f8c4bcf7e5dca77e3f7f9efa743e920
amandasoto01 <amanda.sv01@gmail.com>
2019-10-03 19:55:03 -0500

    created uploadPlane

---
+++ ngx-admin-master/src/app/pages/uploadPlane/uploadPlane.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uploadPlane',
+  templateUrl: './uploadPlane.component.html',
+  styleUrls: ['./uploadPlane.component.css']
+})
+export class UploadPlaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+}
